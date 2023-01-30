b80c2502ba5199437db209b0d4f49db6cb434256
Gabriel Canali <gabrielcanali.dev@gmail.com>
2023-01-30 07:20:06 -0300

    Add: Refactored code and minified versions

--- theme/js/voice-search.js
+++ theme/js/voice-search.js
@@ -1,5 +1,4 @@
 // Verify if browser supports the Web Speech API and if page protocol is HTTPS
-
 if ('webkitSpeechRecognition' in window && window.location.protocol === "https:") {
 
     // If the browser does not support SpeechRecognition, webkitSpeechRecognition is used.
@@ -8,44 +7,32 @@ if ('webkitSpeechRecognition' in window && window.location.protocol === "https:"
     // Define elements selectors
     let form = document.querySelector(".header .header-search-wrapper .input-search");
     let microphone = document.querySelector(".header .header-search-wrapper .button-voice");
-    let micIcon = document.querySelector(".header .header-search-wrapper .button-voice > span");
+    let microphoneIcon = document.querySelector(".header .header-search-wrapper .button-voice > span");
 
-    // Remove "imcompatible" class for microphon
+    // Remove "incompatible" class for microphon
     microphone.classList.remove('incompatible');
     const recognition = new SpeechRecognition();
     recognition.lang = 'pt-Br';
 
     // Defines a function that when speaking performs the search with the result of the speech
-    function onSpeak(e) {
-        
-        micIcon.classList.remove('open');
+    recognition.addEventListener('result', e => {
+        microphoneIcon.classList.remove('open');
         let search = e.results[0][0].transcript;
         form.value = search;
         form.parentNode.submit();
-    }
+    });
     
-    microphone.addEventListener('click', event => {
-
-        micIcon.classList.add('open');
+    // Defines a function that when you click on the microphone it opens
+    microphone.addEventListener('click', () => {
+        microphoneIcon.classList.add('open');
         recognition.start();
-    });
-
-    // Verify if user speak within the 7 second interval, if not close the microphone.
-    let timeoutId;
-    recognition.addEventListener('start', e => {
-
-        timeoutId = setTimeout(function() {
+        // Verify if user speak within the 6 second interval, if not close the microphone.
+        setTimeout(() => {
             recognition.stop();
-        }, 7000);
+            microphoneIcon.classList.remove('open');
+        }, 6000);
     });
 
-    recognition.addEventListener('end', e => {
-
-        clearTimeout(timeoutId);
-        micIcon.classList.remove('open');
-    });
-
-    recognition.addEventListener('result', onSpeak);
 } else {
     console.log('Browser Incompatible with Web Speech API');
 }
